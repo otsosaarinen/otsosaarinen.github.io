12710ebe648081b641d61b4d6e22ec2691d22a54
Otso Saarinen <saarinenotso@gmail.com>
2025-03-02 17:25:44 +0200

    Update particles.js

--- dist/particles.js
+++ dist/particles.js
@@ -1,4 +1,3 @@
-/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load("particles-js", "../particles.json", function () {
-    console.log("particles.js loaded succesfully");
+particlesJS.load("particles-js", "../particles.json", () => {
+    console.log("particles.js loaded successfully");
 });
