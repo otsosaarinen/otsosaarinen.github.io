b78c509f0418b35bed87edf38315d54f401f8a08
Otso Saarinen <saarinenotso@gmail.com>
2025-03-04 17:04:28 +0200

    functions for scrolling back up

--- js/script.js
+++ js/script.js
@@ -5,6 +5,15 @@ document.getElementById("email").addEventListener("click", () => {
 const skillButton = document.getElementById("skills-button");
 const skillList = document.getElementById("skills-container");
 
+// Scroll to skills
 skillButton.addEventListener("click", () => {
     skillList.scrollIntoView({ behavior: "smooth", block: "center" });
+});
+
+// Scroll back up
+const backButton = document.getElementById("back-button");
+const pageContainer = document.getElementById("page-container");
+
+backButton.addEventListener("click", () => {
+    pageContainer.scrollIntoView({ behavior: "smooth", block: "center" });
 });
