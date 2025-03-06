cb4318dffd2b492e6efabf908ad91b3b8c66c110
Otso Saarinen <saarinenotso@gmail.com>
2025-03-06 13:22:33 +0200

    remove arrow controls when screen becomes smaller

--- js/script.js
+++ js/script.js
@@ -16,3 +16,20 @@ arrowDown.addEventListener("click", function () {
         behavior: "smooth",
     });
 });
+
+// Hide arrow controls if screen isn't wide enough
+const arrowControls = document.getElementById("up-down-container");
+
+function toggleArrowControls() {
+    if (window.innerWidth < 800) {
+        arrowControls.style.display = "none";
+    } else {
+        arrowControls.style.display = "flex";
+    }
+}
+
+// Run the function when the page loads
+toggleArrowControls();
+
+// Listen for window resize events
+window.addEventListener("resize", toggleArrowControls);
